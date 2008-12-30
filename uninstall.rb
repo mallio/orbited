49ea80d5bc1939c98923a0347a02a03b39a8fe2e
Mike O'Malley <mike@knockoutapps.com>
2008-12-30 15:31:38 -0600

    Added uninstall hook to remove config files

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,4 @@
-# Uninstall hook code here
+FileUtils.rm File.join(RAILS_ROOT, 'config', 'orbited.yml')
+FileUtils.rm File.join(RAILS_ROOT, 'config', 'orbited.cfg')
+
+puts "Make sure you manually remove any references to Orbited helpers" 
