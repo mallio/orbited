4b698ca005087f7cbf720b6b2446291544b34625
Mike O'Malley <mike@knockoutapps.com>
2009-01-09 16:29:57 -0600

    Added ability to send headers with data, and to not break if the stomp server is down

--- lib/orbited.rb
+++ lib/orbited.rb
@@ -4,10 +4,12 @@ class Orbited
   
   # Sends data in string form to the STOMP server, which sends it to anyone subscribed to 
   # the specified channel
-  def self.send_data(channel, data = nil)
+  def self.send_data(channel, data, headers = {})
     s = Stomp::Client.new
-    s.send(channel, data)
+    s.send(channel, data, headers)
     s.close
+  rescue Errno::ECONNREFUSED
+    RAILS_DEFAULT_LOGGER.error "!!! The Orbited server appears to be down!"
   end
   
 end
